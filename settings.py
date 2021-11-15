6cb8e95476f0d9b3766fc9a8317de8012156d48d
wudlike <46318462+wudlike@users.noreply.github.com>
2021-11-15 14:50:25 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,7 +9,5 @@ class Settings():
         self.samples = 4000
 
         #---------- scan stragegy ---------
-        self.NET = 350  # uk.sqrt(s)
+        self.NET = 300  # uk.sqrt(s)
         self.theta = 80 # deg, zenith angle= theta, and elevation = 90-theta.
-
-        #---------- Ali location ---------
